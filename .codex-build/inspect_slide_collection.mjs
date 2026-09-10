de28a0d2715286e2e84bca34592c658dfa3154db
Toxic5506 <mayankmaster585@gmail.com>
2026-09-10 22:05:03 +0530

    Remove generated Codex build artifacts

--- .codex-build/inspect_slide_collection.mjs
+++
@@ -1,7 +0,0 @@
-import { FileBlob, PresentationFile } from "@oai/artifact-tool";
-const p = await PresentationFile.importPptx(await FileBlob.load("D:\\SIH2026-IDEA-Presentation-Format.pptx"));
-console.log("slides", Object.getOwnPropertyNames(Object.getPrototypeOf(p.slides)));
-console.log("items", p.slides.items.length, Object.getOwnPropertyNames(Object.getPrototypeOf(p.slides.items)));
-console.log("last", Object.getOwnPropertyNames(Object.getPrototypeOf(p.slides.items[6])));
-console.log("remove", p.slides.remove.toString());
-console.log("delete", p.slides.items[6].delete.toString());
